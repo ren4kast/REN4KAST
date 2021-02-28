ea975a420a49924967886394f5ee4cf867de269d
REN4KAST <REN4KAST@gmail.com>
2021-02-28 21:57:51 +0100

    getting data and running model before starting server

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 
 run_with_ngrok(app)  # starts ngrok when the app is run
 
+forecast = get_forecasts_for_today()
+
 @app.route("/")
 def home():
     return "<h1>Renewable Energy Generation Forecasting Service</h1><p>Please use /getForecasts endpoint to get the forecasts.</p>"
 
 @app.route("/getForecasts", methods=["GET"])
 def forecast():
-    forecast = get_forecasts_for_today()
     resp = make_response(forecast.to_csv(columns=forecast.columns))
     resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
     resp.headers["Content-Type"] = "text/csv"
