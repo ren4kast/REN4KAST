9c223554b8ff3daef2dcb907f22128d83e68c20e
ren4kast <77919213+ren4kast@users.noreply.github.com>
2021-12-01 23:15:42 +0100

    Update GenerationDataController.py

--- GenerationDataController.py
+++ GenerationDataController.py
@@ -1,6 +1,31 @@
 from entsoe import EntsoePandasClient
 import pandas as pd
 
+# Getting generation data from entsoe
+def get_entsoe_data(start, end, expected_length):
+    # Defining entsoe key
+    client = EntsoePandasClient("94aa148a-330b-4eee-ba0c-8a5eb0b17825")
+    country_code = 'DE'  # Germany
+
+    # methods that return Pandas Series
+    # 'documentType': 'A75',
+    # 'processType': 'A16',
+    # https://github.com/EnergieID/entsoe-py/blob/5d176699472744c1acef546410826da6549112cf/entsoe/entsoe.py#L270
+    entsoe_data = client.query_generation(country_code, start=start, end=end, psr_type=None)
+
+    # converting to GMT
+    entsoe_data.index = entsoe_data.index.tz_convert('Etc/GMT')
+
+    # Fixing missing and null values for last few datapoints (today)
+    last_dp = entsoe_data.loc[[entsoe_data.index[-1]]]
+    for i in range(expected_length - len(entsoe_data)):
+        entsoe_data = entsoe_data.append(last_dp, ignore_index=False)
+    entsoe_data = entsoe_data.ffill()
+
+    # changing type from non-type to float. because later we want to calculate the average among all cities
+    entsoe_data = entsoe_data.apply(pd.to_numeric)
+    return entsoe_data
+
 # Calculate renewables percentage
 def calculate_renewables_percentage(start, end, expected_length):
     entsoe_data = get_entsoe_data(start, end, expected_length)
