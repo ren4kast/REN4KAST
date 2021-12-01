b71b1b6d7b7b7546fe1b1ca69913ae2737d73008
ren4kast <77919213+ren4kast@users.noreply.github.com>
2021-12-01 22:21:59 +0100

    update based on latest entsoe

--- GenerationDataController.py
+++ GenerationDataController.py
@@ -29,7 +29,7 @@ def get_entsoe_data(start, end, expected_length):
 
 # Calculate renewables percentage
 def calculate_renewables_percentage(start, end, expected_length):
-    entsoe_data = get_entsoe_data(start, end, expected_length)
+    #entsoe_data = get_entsoe_data(start, end, expected_length)
     # asserting that the number of rows
     assert expected_length == len(entsoe_data), "Number of rows did NOT match! {} vs. {}".format(expected_length,
                                                                                                  len(entsoe_data))
@@ -40,26 +40,27 @@ def calculate_renewables_percentage(start, end, expected_length):
 
     generation_data = entsoe_data
     sumBioMassAndHydro = generation_data['Biomass'] + generation_data['Hydro Run-of-river and poundage'] + \
-                         generation_data[
+                         generation_data['Hydro Pumped Storage'] + generation_data[
                              'Hydro Water Reservoir'] + generation_data['Geothermal'] + generation_data['Waste']
 
     sumOthers = generation_data['Wind Offshore'] + generation_data['Wind Onshore'] + generation_data['Solar'] + \
                 generation_data['Nuclear'] + generation_data['Fossil Brown coal/Lignite'] + generation_data[
-                    'Fossil Hard coal'] + generation_data['Fossil Gas'] + generation_data['Hydro Pumped Storage'] + \
-                generation_data['Other'] + generation_data['Other renewable'] + generation_data['Fossil Oil']
+                    'Fossil Hard coal'] + generation_data['Fossil Gas'] + \
+                generation_data['Other'] + generation_data['Other renewable'] + generation_data['Fossil Oil'] + \
+                generation_data['Fossil Coal-derived gas']
 
     calcTotal = sumBioMassAndHydro + sumOthers
 
     RenForecast = generation_data.drop(columns=['Biomass', 'Fossil Brown coal/Lignite', 'Fossil Gas',
-                                                'Fossil Hard coal', 'Fossil Oil', 'Geothermal', 'Hydro Pumped Storage',
+                                                'Fossil Hard coal', 'Fossil Oil','Fossil Coal-derived gas', 'Geothermal', 'Hydro Pumped Storage',
                                                 'Hydro Run-of-river and poundage', 'Hydro Water Reservoir', 'Nuclear',
                                                 'Other', 'Waste', #('Other renewable', 'Actual Consumption'),
-                                                ('Solar', 'Actual Consumption'),
+                                                ('Solar', 'Actual Consumption'), ('Other renewable', 'Actual Consumption'),
                                                 ('Wind Onshore', 'Actual Consumption')])
 
     RenForecast.insert(0, "calcTotal", calcTotal["Actual Aggregated"], True)
     RenForecast.insert(1, "sumBioMassAndHydro", sumBioMassAndHydro["Actual Aggregated"], True)
-
+    
     RenForecast.columns = ["calcTotal", "sumBioMassAndHydro", "Other renewable", "Solar", "Wind Offshore",
                            "Wind Onshore"]
 
